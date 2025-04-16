bfe0d72b37cc6f5e748eea3d40a1a8253f546ce4
paulj222 jo <pauljo121400@gmail.com>
2025-04-16 17:35:41 +0000

    started

--- index.js
+++ index.js
@@ -16,21 +16,38 @@ const app = express();
 app.use(express.json());
 const config = {
   apiversion: "1",
-  author: "",       // TODO: Your Battlesnake Username
+  author: "paulj222",       // TODO: Your Battlesnake Username
   color: "#cb82ff", // TODO: Choose color
-  head: "default",  // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
-  tail: "default",  // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
+  head: "tiger-king",  // TODO: Choose head, see https://play.battlesnake.com/customizations/ for options unlocked in your account
+  tail: "rbc-necktie",  // TODO: Choose tail, see https://play.battlesnake.com/customizations/ for options unlocked in your account
 }
 
 //TODO: respond to GET requests on "/" with the config object above
-
+app.get("/",(request, response) => {
+  response.json(config)
+})
 //TODO: respond to POST requests on "/start". Your response itself is ignored, but must have status code "200"
 //      the request body will contain objects representing the game instance, game board state, and your snake
 //      https://docs.battlesnake.com/api/requests/start
+function handleStart(request, response) {
+  let gameData = request.body
 
+  console.log('start')
+  response.status(200).serd('ok')
+}
 //TODO: respond to POST requests on "/move". Your response should be an object with a "move" property and optionally
 //      a "shout" property. The request body again contains objects representing the game state
 //      https://docs.battlesnake.com/api/requests/move
+function handlemove(request, response) {
+  let gameData = request.body
+
+  let safeMove = ['up','down','left','right']
+  let move = safeMove[Math.floor(Math.random()*safeMove.length)]
+  console.log('move'+ move)
+  response.status(200).send({
+    move:move
+  })
+} 
 
 //TODO: respond to POST requests on "/end", which signals the end of a game. Your response itself is ignored, 
 //      but must have status code "200" the request body will contain objects representing the game
